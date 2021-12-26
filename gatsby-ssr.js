279d45538611b557af209174f7bf0f4c2e474718
Justin Linn <justinslinn727@gmail.com>
2021-12-26 11:43:36 -0500

    Add postcss back

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,3 +6,36 @@
 
 // internal import
 require('./src/tailwind.css');
+// const { setup } = require('twind');
+// const { asyncVirtualSheet, getStyleTagProperties } = require('twind/server');
+// const React = require('react');
+
+// const sheet = asyncVirtualSheet();
+
+// setup({ sheet });
+
+// exports.wrapPageElement = ({ element }) => {
+//   sheet.reset();
+
+//   return element;
+// };
+
+// exports.onRenderBody = ({ setHeadComponents, pathname }) => {
+//   const { id, textContent } = getStyleTagProperties(sheet);
+
+//   const styleProps = {
+//     id,
+//     dangerouslySetInnerHTML: {
+//       __html: textContent,
+//     },
+//   };
+
+//   setHeadComponents([
+//     React.createElement('style', {
+//       id,
+//       dangerouslySetInnerHTML: {
+//         __html: textContent,
+//       },
+//     }),
+//   ]);
+// };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
