2f03b5f902784449da07babe8747469be353b786
Justin Linn <justinslinn727@gmail.com>
2021-12-26 16:43:59 -0500

    Convert hero to twind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
