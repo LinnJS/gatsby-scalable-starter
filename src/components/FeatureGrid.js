277236da9ede78015072324a1ab2e8a0d9ff8014
Justin Linn <justinslinn727@gmail.com>
2021-12-26 16:49:20 -0500

    Migrade grid to twind

--- src/components/FeatureGrid.js
+++ src/components/FeatureGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { tw } from 'twind';
 
 import {
   CloudUploadIcon,
@@ -45,29 +46,31 @@ const features = [
 
 const FeatureGrid = () => {
   return (
-    <section className="relative py-16 bg-white sm:py-24 lg:py-32">
-      <div className="max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-        <h2 className="text-base font-semibold tracking-wider uppercase text-cyan-600">Deploy faster</h2>
-        <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+    <section className={tw`relative py-16 bg-white sm:py-24 lg:py-32`}>
+      <div className={tw`max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl`}>
+        <h2 className={tw`text-base font-semibold tracking-wider uppercase text-cyan-600`}>Deploy faster</h2>
+        <p className={tw`mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl`}>
           Everything you need to deploy your app
         </p>
-        <p className="mx-auto mt-5 text-xl text-gray-500 max-w-prose">
+        <p className={tw`mx-auto mt-5 text-xl text-gray-500 max-w-prose`}>
           Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
           malesuada. Eleifend condimentum id viverra nulla.
         </p>
-        <div className="mt-12">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        <div className={tw`mt-12`}>
+          <div className={tw`grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3`}>
             {features.map((feature) => (
-              <div key={feature.name} className="pt-6">
-                <div className="flow-root px-6 pb-8 rounded-lg bg-gray-50">
-                  <div className="-mt-6">
+              <div key={feature.name} className={tw`pt-6`}>
+                <div className={tw`flow-root px-6 pb-8 rounded-lg bg-gray-50`}>
+                  <div className={`-mt-6`}>
                     <div>
-                      <span className="inline-flex items-center justify-center p-3 rounded-md shadow-lg bg-gradient-to-r from-teal-500 to-cyan-600">
-                        <feature.icon className="w-6 h-6 text-white" aria-hidden="true" />
+                      <span
+                        className={tw`inline-flex items-center justify-center p-3 rounded-md shadow-lg bg-gradient-to-r from-teal-500 to-cyan-600`}
+                      >
+                        <feature.icon className={tw`w-6 h-6 text-white`} aria-hidden="true" />
                       </span>
                     </div>
-                    <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">{feature.name}</h3>
-                    <p className="mt-5 text-base text-gray-500">{feature.description}</p>
+                    <h3 className={tw`mt-8 text-lg font-medium tracking-tight text-gray-900`}>{feature.name}</h3>
+                    <p className={tw`mt-5 text-base text-gray-500`}>{feature.description}</p>
                   </div>
                 </div>
               </div>
